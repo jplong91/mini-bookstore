941e2080c58c8267467ad4b533d783422fdfbcaa
John Long <jplong91@gmail.com>
2017-11-14 18:35:45 -0600

    db seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Book.create(title: "The Way of Kings", author: "Brandon Sanderson", price: "10.00", pages: "1,007", image: "https://images-na.ssl-images-amazon.com/images/I/91FZ41RwZzL.jpg")
+# Book.create(title: "The Eye of the World", author: "Robert Jordan", price: "8.00", pages: "685", image: "https://upload.wikimedia.org/wikipedia/en/0/00/WoT01_TheEyeOfTheWorld.jpg")
+# Book.create(title: "The Name of the Wind", author: "Patrick Rothfuss", price: "7.50", pages: "662", image: "https://images.gr-assets.com/books/1472068073l/186074.jpg")
